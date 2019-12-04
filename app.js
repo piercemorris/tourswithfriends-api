2253f7975ca4f30ce4af7b5602aabe2d72822f30
Pierce Morris <pierce.morris1998@gmail.com>
2019-12-04 10:28:20 +0000

    add firebase-admin

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var Firebase = require("firebase-admin");
+const FirebaseConnection = require("./firebase/index");
 
 var app = express();
+FirebaseConnection.init();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
---
+++ firebase/index.js
@@ -0,0 +1,19 @@
+const Firebase = require("firebase-admin");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDUBTicIY6MlpuCptBwr3G15Pfdh1z6PwU",
+  authDomain: "city-tours-with-friends.firebaseapp.com",
+  databaseURL: "https://city-tours-with-friends.firebaseio.com",
+  projectId: "city-tours-with-friends",
+  storageBucket: "city-tours-with-friends.appspot.com",
+  messagingSenderId: "1091067476362",
+  appId: "1:1091067476362:web:6d597d1310c82d9ba13073",
+  measurementId: "G-36X23K6Z6S"
+};
+
+const init = () => {
+  const connection = Firebase.initializeApp(firebaseConfig);
+  if (connection) console.log("Firebase connected @" + Date.now());
+};
+
+exports.init = init;
