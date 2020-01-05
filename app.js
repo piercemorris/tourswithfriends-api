2a6eaa1a1027aad8fc8511549514cd6ef3fe6fce
Pierce Morris <pierce.morris1998@gmail.com>
2020-01-05 16:02:13 +0000

    add file

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var Firebase = require("firebase-admin");
 const FirebaseConnection = require("./firebase/index");
 
 var app = express();
 FirebaseConnection.init();
 
-app.use(logger("dev"));
+app.use(logger("prod"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
